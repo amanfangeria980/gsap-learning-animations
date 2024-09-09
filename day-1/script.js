844afa48d408f6972323fdf89f6013c7a3a17253
Aman Fangeria <aman.fangeria@geekyants.com>
2024-09-09 16:38:38 +0530

    stagger, repeat and yoyo property

--- day-1/script.js
+++ day-1/script.js
@@ -13,4 +13,18 @@ gsap.from("#box2", {
     x: 1200,
     duration: 2,
     delay: 1,
+    // repeat property,if we put 1, now it will run 2 times, for infinite use -1
+    repeat: -1,
+    // to repeat and bounce back,reverse
+    yoyo: true,
+});
+
+gsap.from("h1", {
+    color: "red",
+    y: 20,
+    duration: 2,
+    delay: 1,
+    opacity: 0,
+    // put the animations on the same type of tags one by one after each second specified by number, also if we give -1 it will happen each after 1 seconds in reverse
+    stagger: 1,
 });
