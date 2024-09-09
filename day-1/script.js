701f76258006982cd0331544dcad07d86cb6bf33
Aman Fangeria <aman.fangeria@geekyants.com>
2024-09-09 18:30:07 +0530

    gsap timeline - 1

--- day-1/script.js
+++ day-1/script.js
@@ -28,3 +28,48 @@ gsap.from("h1", {
     // put the animations on the same type of tags one by one after each second specified by number, also if we give -1 it will happen each after 1 seconds in reverse
     stagger: 1,
 });
+
+// here we have to specifically calculate delays, if we want the boxes to animate after each other, here comes the timeline...
+// gsap.to("#box3", {
+//     x: 1500,
+//     rotate: 360,
+//     duration: 1.5,
+//     delay: 1,
+// });
+
+// gsap.to("#box4", {
+//     x: 1500,
+//     backgroundColor: "red",
+//     duration: 1.5,
+//     delay: 2.5,
+// });
+// gsap.to("#box5", {
+//     x: 1500,
+//     scale: 0.5,
+//     borderRadius: "50%",
+//     duration: 1.5,
+//     delay: 4,
+// });
+
+// timeline
+
+var tl = gsap.timeline();
+
+tl.to("#box3", {
+    x: 1500,
+    rotate: 360,
+    duration: 1.5,
+    delay: 1,
+});
+
+tl.to("#box4", {
+    x: 1500,
+    backgroundColor: "red",
+    duration: 1.5,
+});
+tl.to("#box5", {
+    x: 1500,
+    scale: 0.5,
+    borderRadius: "50%",
+    duration: 1.5,
+});
