d2799d5ba5fa34d2d90885c399205f5afae48653
Aman Fangeria <aman.fangeria@geekyants.com>
2024-09-13 12:29:22 +0530

    created the cursor change on image hover and used overlay to stop jittering the text in the animation

--- day-4/script.js
+++ day-4/script.js
@@ -1,12 +1,28 @@
 const main = document.querySelector("#main");
 const cursor = document.querySelector("#cursor");
+const imageDiv = document.querySelector("#image");
 
 main.addEventListener("mousemove", function (dets) {
     // console.log(dets);
     gsap.to(cursor, {
         x: dets.x,
         y: dets.y,
-        duration: 0.3,
+        duration: 0.6,
         easing: "elastic.out(1,0.3)",
+    });
+});
+
+imageDiv.addEventListener("mouseenter", function () {
+    cursor.innerHTML = "View More!";
+    gsap.to(cursor, {
+        scale: 4,
+        backgroundColor: "#ffffff8a",
+    });
+});
+imageDiv.addEventListener("mouseleave", function () {
+    cursor.innerHTML = "";
+    gsap.to(cursor, {
+        scale: 1,
+        backgroundColor: "white",
     });
 });
